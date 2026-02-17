1ebc86cdd519480cf0f2570eb462de618a0016c2
Farhan <farhanaliforwardsports@gmail.com>
2026-02-17 11:54:12 +0500

    initial commit version.2

---
+++ src/test/java/com/example/master_data_service/MasterDataServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.master_data_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MasterDataServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
